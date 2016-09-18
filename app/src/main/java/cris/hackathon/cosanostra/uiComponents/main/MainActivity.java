79b7e318d270998a6cb757f675576471e1447d69
Tomás Ferraro <tetferraro@gmail.com>
2016-09-18 06:50:57 -0300

    se puede mover de la activity login a la main, y viceversa (al volver de la main a la login, la cuenta sigue logueada)

---
+++ app/src/main/java/cris/hackathon/cosanostra/uiComponents/main/MainActivity.java
@@ -0,0 +1,58 @@
+package cris.hackathon.cosanostra.uiComponents.main;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+
+import cris.hackathon.cosanostra.LoginActivity;
+import cris.hackathon.cosanostra.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    Button button2;
+    Button button3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.button);
+        button2 = (Button) findViewById(R.id.button2);
+        button3 = (Button) findViewById(R.id.button3);
+
+        button.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View arg0)
+            {
+                //llevar a otro lado (crear intent)
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View arg0)
+            {
+                //llevar a otro lado (crear intent)
+            }
+        });
+
+        button3.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View arg0)
+            {
+
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+
+
+
+            }
+        });
+
+
+    }
+}
